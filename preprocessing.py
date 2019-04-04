cf1bc5276bc30ef65c94e9e81cc75ee77c751a3d
Wong Zhiwei <sherman2zhiwei@yahoo.com>
2019-04-05 03:44:43 +0800

    Created file for baseline algorithm

--- preprocessing.py
+++ preprocessing.py
@@ -29,12 +29,13 @@ def string_to_list(string_list):
 	if len(string_list) > 2:
 		# print(string_list)
 		string_list[1] = string_list[1][:-1]
-		string_list[2] = string_list[2][1:]
+		string_list = string_list[:2]
 		for i in range(len(string_list)):
 			string_list[i] = string_list[i][1:-1]
 		return string_list
 
 	if len(string_list) > 1:
+		string_list = string_list[:1]
 		string_list[0] = string_list[0][:-1]
 		string_list[1] = string_list[1][1:] 
 
