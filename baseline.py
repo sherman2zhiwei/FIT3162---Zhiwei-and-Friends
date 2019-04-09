f1aeb7704d55001f5bbb7f36f2266250b5e85db8
Ho Kong Zheng <z@Anonimus.local>
2019-04-09 22:01:21 +0800

    added 2 SemEval Restaurant Train Dataset 2016

--- baseline.py
+++ baseline.py
@@ -1,3 +1,13 @@
 import nltk
+from preprocessing import *
+
 def return_nouns(review):
-	
+    for word, pos in nltk.pos_tag(review):
+        if pos in ['NN', "NNP"]:
+            nouns.add(word)
+    return nouns
+
+if __name__ == "__main__":
+    # data = readfile()
+    # review_list = list(map(string_to_list, data))
+    print(return_nouns("Just like home"))
--- preprocessing.py
+++ preprocessing.py
@@ -1,71 +1,33 @@
 import nltk
 import pandas
 
-# tokens = nltk.word_tokenize("KZ can you please shut the fuck up.")
+def readfile():
+    filename = "./TA_restaurants_curated.csv"
+    data = pandas.read_csv(filename, sep=",", encoding="cp1252")
+    data = data.dropna(subset=["Reviews"])
+    data = data[data.Reviews != "[[], []]"]
 
-# print("Parts of Speech: ", nltk.pos_tag(tokens))
+    start, stop, step = 2,-2,1
 
-
-filename = "./TA_restaurants_curated.csv"
-data = pandas.read_csv(filename, sep=",", encoding="cp1252")
-
-
-# print(len(data))
-data = data.dropna(subset=["Reviews"])
-# print(len(data))
-# print(len(data))
-data = data[data.Reviews != "[[], []]"]
-# print(len(data))
-# print(data["Reviews"].head(3)[0])
-
-start, stop, step = 2,-2,1
-
-data["Reviews"] = data["Reviews"].astype(str)
-data["Reviews"] = data["Reviews"].str.slice(start, stop, step)
-data["Reviews"] = data["Reviews"].str.split(", ")
+    data["Reviews"] = data["Reviews"].astype(str)
+    data["Reviews"] = data["Reviews"].str.slice(start, stop, step)
+    data["Reviews"] = data["Reviews"].str.split(", ")
+    return data
 
 def string_to_list(string_list):
-	if len(string_list) > 2:
-		#print(string_list)
-		string_list[1] = string_list[1][:-1]
-		string_list = string_list[:2]
-		for i in range(len(string_list)):
-			string_list[i] = string_list[i][1:-1]
-		return string_list
-
-	if len(string_list) > 1:
-		print(string_list)
-
-		string_list = string_list[:1]
-		string_list[0] = string_list[0][:-1]
-		for i in range(len(string_list)):
-			string_list[i] = string_list[i][1:-1]
-		# string_list[1] = string_list[1][1:] 
-		print(string_list)
-
-data["Reviews"] = data["Reviews"].apply(string_to_list)
-
-
-
-
-# for i in range(len(data)):
-# 	data["Reviews"][i] = data["Reviews"][i][1:-1]
-
-# print(data["Reviews"].head(3)[0])
-
-
-
-
-
-
-
-
-# File = open(fileName) #open file
-# lines = File.read() #read all lines
-# sentences = nltk.sent_tokenize(lines) #tokenize sentences
-# nouns = [] #empty to array to hold all nouns
-
-# for sentence in sentences:
-#      for word,pos in nltk.pos_tag(nltk.word_tokenize(str(sentence))):
-#          if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
-#              nouns.append(word)
+    if len(string_list) > 2:
+        string_list[1] = string_list[1][:-1]
+        string_list = string_list[:2]
+        for i in range(len(string_list)):
+            string_list[i] = string_list[i][1:-1]
+    else:
+        string_list = string_list[:1]
+        string_list[0] = string_list[0][:-1]
+        for i in range(len(string_list)):
+            string_list[i] = string_list[i][1:-1]
+    return string_list
+    
+if __name__ == "__main__":
+    data = readfile()
+    review_list = list(map(string_to_list, data["Reviews"]))
+    print(review_list[0])
